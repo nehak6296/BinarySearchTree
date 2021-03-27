ec4d49c2b124f2d45757f178565edf1581ae51a5
nehak6296 <nehak6296@gmail.com>
2021-03-28 00:46:38 +0530

    [Neha]Added:Created a binary search tree with root node 56 and then added 30,70

---
+++ BinarySearchTree/BinarySearchTree/BinarySearchTreeDemo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    class BinarySearchTreeDemo<T> where T : IComparable<T>
+    {
+        public T NodeData { get; set; }
+        public BinarySearchTreeDemo<T> leftTree { get; set; }
+        public BinarySearchTreeDemo<T> rightTree { get; set; }
+
+        public BinarySearchTreeDemo(T nodeData)
+        {
+            this.NodeData = nodeData;
+            this.rightTree = null;
+            this.leftTree = null;
+
+        }
+
+        int leftCount = 0, rightCount = 0;
+        bool result = false;
+        public void Insert(T item)
+        {
+            T currentNodeValue = this.NodeData;
+            if ((currentNodeValue.CompareTo(item)) > 0)
+            {
+                if (this.leftTree == null)
+                    this.leftTree = new BinarySearchTreeDemo<T>(item);
+                else
+                    this.leftTree.Insert(item);
+            }
+            else
+            {
+                if (this.rightTree == null)
+                    this.rightTree = new BinarySearchTreeDemo<T>(item);
+                else
+                    this.rightTree.Insert(item);
+            }
+        }
+
+
+        public void Display()
+        {
+            if (this.leftTree != null)
+            {
+                this.leftCount++;
+                this.leftTree.Display();
+
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if (this.rightTree != null)
+            {
+                this.rightCount++;
+                this.rightTree.Display();
+            }
+        }
+    }
+}
--- BinarySearchTree/BinarySearchTree/Program.cs
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -7,6 +7,10 @@ namespace BinarySearchTree
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Binary Search Tree Demo!");
+            BinarySearchTreeDemo<int> binarSearchTree = new BinarySearchTreeDemo<int>(56);
+            binarSearchTree.Insert(30);
+            binarSearchTree.Insert(70);
+            binarSearchTree.Display();
         }
     }
 }
