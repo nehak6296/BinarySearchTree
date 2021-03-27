9c48aa82be58f86a112c54fdb63c40dba5c07cea
nehak6296 <nehak6296@gmail.com>
2021-03-28 01:02:03 +0530

    [Neha]Added: Inserted Elements in tree and checking size

--- BinarySearchTree/BinarySearchTree/BinarySearchTreeDemo.cs
+++ BinarySearchTree/BinarySearchTree/BinarySearchTreeDemo.cs
@@ -55,5 +55,9 @@ namespace BinarySearchTree
                 this.rightTree.Display();
             }
         }
+        public void GetSize()
+        {
+            Console.WriteLine("Size" + " " + (1 + this.leftCount + this.rightCount));
+        }
     }
 }
--- BinarySearchTree/BinarySearchTree/Program.cs
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -10,7 +10,19 @@ namespace BinarySearchTree
             BinarySearchTreeDemo<int> binarSearchTree = new BinarySearchTreeDemo<int>(56);
             binarSearchTree.Insert(30);
             binarSearchTree.Insert(70);
+            binarSearchTree.Insert(22);
+            binarSearchTree.Insert(40);
+            binarSearchTree.Insert(60);
+            binarSearchTree.Insert(95);
+            binarSearchTree.Insert(11);
+            binarSearchTree.Insert(65);
+            binarSearchTree.Insert(3);
+            binarSearchTree.Insert(16);
+            binarSearchTree.Insert(63);
+            binarSearchTree.Insert(67);            
             binarSearchTree.Display();
+            binarSearchTree.GetSize();
+            Console.ReadKey();
         }
     }
 }
